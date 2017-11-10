c427bec5acde79c44ffae12300ca9828e7c7dd8e
Jennifer Young <jen.l.young@gmail.com>
2017-11-10 14:50:36 -0600

    add variables row and column using Jquery

--- js/designs.js
+++ js/designs.js
@@ -1,3 +1,4 @@
+
 // Select color input
 // Select size input
 
@@ -5,21 +6,35 @@
 
 // function makeGrid(rows, columns) {
 
-$(function() {
-    var row = 5;
-    var column = 5;
-    for (var i = 0; i <= row; i++) {
-        $('#pixel_canvas').append('<tr id=rows></tr>');
-        for (var c = 0; c <= column; c++) {
-            $('tr:last').append('<td id=columns></td>');
-        }
-    }
-});
 
+
+function makeGrid(){
+    //variables set using JQuery
+    var row = $('#input_height').val();
+    var column = $('#input_width').val();
+    // nested for loop that adds rows and then columns
+        for (var i = 0; i < row; i++) {
+            $('#pixel_canvas').append('<tr id=rows></tr>');
+            for (var c = 0; c <= column; c++) {
+                $('tr:last').append('<td id=columns></td>');
+            };
+        };
+}
+// event listener that changes the color of the cell when clicked on
 $('#pixel_canvas').on('click', function(evt){
     var color = $('#colorPicker').val();
     $(evt.target).css('background', color);
 });
+//
+makeGrid();
+
+
+
+
+    // $('#input_height').change(function(){
+    //     var val = $(this).val();
+    //     $('#input_height[value="1"]').val(value);
+    // });
 
 
 

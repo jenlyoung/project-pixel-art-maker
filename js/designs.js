df7a6f25cba39709bfb2762d7d5a4847c05d05ad
Jennifer Young <jen.l.young@gmail.com>
2017-11-13 13:38:51 -0600

    Add event listener for submit button and fix width and height variables git commit -m Add event listener for submit button and fix width and height variables

--- js/designs.js
+++ js/designs.js
@@ -4,23 +4,26 @@
 
 // When size is submitted by the user, call makeGrid()
 
-// function makeGrid(rows, columns) {
-
-
 
 function makeGrid(){
-    //variables set using JQuery
-    var row = $('#input_height').val();
-    var column = $('#input_width').val();
-    // nested for loop that adds rows and then columns
+    // set the variable to the user input when "submit" clicked
+    $('input[type="submit"]').on('click', (function() {
+        var row = $('#input_height').val();
+        var column = $('#input_width').val();
+
+    //  nested for loop that adds rows and then columns
         for (var i = 0; i < row; i++) {
             $('#pixel_canvas').append('<tr id=rows></tr>');
-            for (var c = 0; c <= column; c++) {
+            for (var c = 0; c < column; c++) {
                 $('tr:last').append('<td id=columns></td>');
             };
         };
-}
-// event listener that changes the color of the cell when clicked on
+    // prevents the grid from disappearing
+        event.preventDefault();
+    }));
+};
+
+//event listener that changes the color of the cell when clicked on
 $('#pixel_canvas').on('click', function(evt){
     var color = $('#colorPicker').val();
     $(evt.target).css('background', color);
@@ -30,11 +33,6 @@ makeGrid();
 
 
 
-
-    // $('#input_height').change(function(){
-    //     var val = $(this).val();
-    //     $('#input_height[value="1"]').val(value);
-    // });
 
 
 

131eb5dce0161a832a9c0b6483668e68c963419f
Jennifer Young <jen.l.young@gmail.com>
2017-11-13 22:05:00 -0600

    Function works

--- js/designs.js
+++ js/designs.js
@@ -1,31 +1,31 @@
 
-function makeGrid(){
+function makeGrid() {
 
     // set the variable to the user input when "submit" clicked
-    $('input[type="submit"]').on('click', (function() {
-        // removes previous user input for table size
-        $("tr").detach();
-
+    $('input[type="submit"]').on('click', (function () {
         const row = $('#input_height').val();
         const column = $('#input_width').val();
 
-    //  nested for loop that adds rows and then columns
+        // removes previous user input for table size
+        $('tr').detach();
+        
+        // nested for loop that adds rows and then columns
         for (let i = 0; i < row; i++) {
             $('#pixel_canvas').append('<tr id=rows></tr>');
             for (let c = 0; c < column; c++) {
                 $('tr:last').append('<td id=columns></td>');
-            };
-        };
-    // prevents the grid from disappearing
+            }
+        }
+        // prevents the grid from disappearing
         event.preventDefault();
     }));
 
     //event listener that changes the color of the cell when clicked on
-    $('#pixel_canvas').on('click', function(evt){
-        var color = $('#colorPicker').val();
+    $('#pixel_canvas').on('click', function (evt) {
+        const color = $('#colorPicker').val();
         $(evt.target).css('background', color);
-    });
-};
+    })
+}
 
 makeGrid();
 

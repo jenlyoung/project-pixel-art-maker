eae62e86eef99ad32268a3fcf2a32d9fdb26a2c5
Jennifer Young <jen.l.young@gmail.com>
2017-11-13 23:23:34 -0600

    Moved event listeners ooutside function, call makeGrid inside event listener, fixed variables

--- js/designs.js
+++ js/designs.js
@@ -1,33 +1,36 @@
+const table = $('#pixel_canvas');
 
 function makeGrid() {
 
     // set the variable to the user input when "submit" clicked
-    $('input[type="submit"]').on('click', (function () {
-        const row = $('#input_height').val();
-        const column = $('#input_width').val();
+    const row = $('#input_height').val();
+    const column = $('#input_width').val();
 
-        // removes previous user input for table size
-        $('tr').detach();
-        
-        // nested for loop that adds rows and then columns
-        for (let i = 0; i < row; i++) {
-            $('#pixel_canvas').append('<tr id=rows></tr>');
-            for (let c = 0; c < column; c++) {
-                $('tr:last').append('<td id=columns></td>');
-            }
+    table.children().detach();
+
+    // nested for loop that adds rows and then columns
+    for (let i = 0; i < row; i++) {
+        table.append('<tr></tr>');
+        for (let c = 0; c < column; c++) {
+            $('tr:last').append('<td></td>');
         }
-        // prevents the grid from disappearing
-        event.preventDefault();
-    }));
-
-    //event listener that changes the color of the cell when clicked on
-    $('#pixel_canvas').on('click', function (evt) {
-        const color = $('#colorPicker').val();
-        $(evt.target).css('background', color);
-    })
+    }
 }
 
-makeGrid();
+// event listener that calls madeGrid() when the "submit" button is clicked
+$('input[type="submit"]').on('click', (function () {
+    makeGrid();
+    // prevents the grid from disappearing
+    event.preventDefault();
+}));
+
+// event listener that changes the color of the cell when clicked on
+table.on('click', function (evt) {
+    const color = $('#colorPicker').val();
+    $(evt.target).css('background', color);
+})
+
+
 
 
 
